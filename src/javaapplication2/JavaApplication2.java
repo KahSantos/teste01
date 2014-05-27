b97fc9534c7a1f6bc6153c6c58edf38d7dd2ae84
karen_luiz <karen_luiz@10.1.48.250>
2014-05-27 13:43:52 -0300

    Inicialização de um projeto

---
+++ src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author karen_luiz
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+        JOptionPane.showMessageDialog(null, "Testado com Louvor");
+        
+    }
+}
